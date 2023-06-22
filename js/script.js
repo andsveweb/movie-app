a726670a2c83170c579bf177f9d1a09ead0934fe
andreas svensson <andsveweb@gmail.com>
2023-06-22 12:09:56 +0200

    Added popular tv shows

--- js/script.js
+++ js/script.js
@@ -34,6 +34,39 @@ async function displayPopularMovies() {
     })
 }
 
+// display popular tv-shows
+async function displayPopularShows() {
+    const { results } = await fetchAPIData('tv/popular');
+    
+    results.forEach((show) => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        <a href="tv-details.html?id=${show.id}">
+          ${show.poster_path
+            ? `<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+          />` : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="Movie Title"
+        />`
+        }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Air Date: ${show.first_air_date}</small>
+          </p>
+        </div>
+        `;
+
+        document.querySelector('#popular-shows').appendChild(div)
+    })
+}
+
 // Fetch from "the movie data base"
 async function fetchAPIData(endpoint) {
     // Register your key at https://www.themoviedb.org
@@ -77,7 +110,7 @@ function init() {
      displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
